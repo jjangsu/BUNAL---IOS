54bf762515a65d24c6b0f9af2133a8eb82d6924b
SuHyunJang <jjangsu1104@gmail.com>
2020-06-09 18:23:46 +0900

    정류소 리스트 보여줌 한글이 들어갈 수 있도록 함

--- BuNal/BuNal/SearchViewController.swift
+++ BuNal/BuNal/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, XMLParserDelegate {
+class SearchViewController: UIViewController, XMLParserDelegate, UITableViewDataSource {
     
     @IBOutlet weak var micButton: UIButton!
     @IBOutlet weak var searchButton: UIButton!
@@ -21,8 +21,8 @@ class SearchViewController: UIViewController, XMLParserDelegate {
     var posts = NSMutableArray()
     var elements = NSMutableDictionary()
     var element = NSString()
-    var name = NSMutableString()
-    
+    var stationName = NSMutableString()
+    var SiName = NSMutableString()
     
     @IBAction func micButtonAction(_ sender: Any) {
     }
@@ -43,8 +43,17 @@ class SearchViewController: UIViewController, XMLParserDelegate {
     
     func beginXmlFileParsing()
     {
+        // 경기도꺼
+        // let path = "https://openapi.gg.go.kr/BusStation?ServiceKey=b84002c9970245a8b2e12f849ed7f049&SIGUN_NM="
+        let path = "http://openapi.gbis.go.kr/ws/rest/busstationservice?serviceKey=cOXFXk2qE%2FhuIiYcsMQ4gv032heBUTwuP%2FDQwW0TskxrWGtrdVC6bJPNmJ2CbVcFq6P1eirV9X5d5fql75eeRg%3D%3D&keyword="
+        let name = "강남"
+        
+        // let fullPath = path + name
+        let quaryPath = name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!    // urlQueryAllowed
+        print(quaryPath)
+        
         posts = []
-        parser = XMLParser(contentsOf:(URL(string:"https://openapi.gg.go.kr/BusStation?ServiceKey=b84002c9970245a8b2e12f849ed7f049"))!)!
+        parser = XMLParser(contentsOf:(URL(string: path + quaryPath  ))!)!
         
         parser.delegate = self
         
@@ -58,48 +67,63 @@ class SearchViewController: UIViewController, XMLParserDelegate {
         
         listTableView!.reloadData()
     }
-    
-    func beginParsing()
-    {
-        posts = []
-        parser = XMLParser(contentsOf:(URL(string:"http://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberCurrStateList?ServiceKey=sea100UMmw23Xycs33F1EQnumONR%2F9ElxBLzkilU9Yr1oT4TrCot8Y2p0jyuJP72x9rG9D8CN5yuEs6AS2sAiw%3D%3D"))!)!
 
-        parser.delegate = self
-        parser.parse()
-        listTableView!.reloadData()
-    }
     
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String]) {
         element = elementName as NSString
         
         // print("CurrentElementl: [\(elementName)]")
         
-        if ( elementName as NSString ).isEqual(to: "row")
+        if ( elementName as NSString ).isEqual(to: "busStationList")
         {
             elements = NSMutableDictionary()
             elements = [:]
-            name = NSMutableString()
-            name = ""
+            stationName = NSMutableString()
+            stationName = ""
+            SiName = NSMutableString()
+            SiName = ""
         }
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String)
     {
-        if element.isEqual(to: "STATION_NM_INFO"){
-            name.append(string)
+        if element.isEqual(to: "stationName"){
+            stationName.append(string)
         }
-        
+        else if element.isEqual(to: "regionName") {
+            SiName.append(string)
+        }
+        print(string)
     }
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI namspaceURI: String?, qualifiedName qName: String?)
     {
-        if(elementName as NSString).isEqual(to: "row") {
-            if !name.isEqual( nil) {
-                elements.setObject(name, forKey: "STATION_NM_INFO" as NSCopying)
+        if(elementName as NSString).isEqual(to: "busStationList") {
+            if !stationName.isEqual( nil) {
+                elements.setObject(stationName, forKey: "stationName" as NSCopying)
+            }
+            if !SiName.isEqual( nil) {
+                elements.setObject(SiName, forKey: "regionName" as NSCopying)
             }
             
             posts.add(elements)
+            print(posts)
         }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = (posts.object(at: indexPath.row) as AnyObject).value(forKey: "stationName") as! NSString as String
+        cell.detailTextLabel?.text = (posts.object(at: indexPath.row) as AnyObject).value(forKey: "regionName") as! NSString as String
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // print(posts.count)
+        return posts.count
     }
 
 }
